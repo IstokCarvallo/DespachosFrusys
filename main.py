fd701fcba9caf83cfefcd8cc126a5b29eb586225
IstokCarvallo <51374996+IstokCarvallo@users.noreply.github.com>
2024-10-01 17:25:56 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import datetime
+
+# Inicio de programa
+if __name__ == '__main__':
+    print(f'Inicio proceso de carga :{datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}')
+    
+    print(f'Termino proceso de carga :{datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}')

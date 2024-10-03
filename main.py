dbeff8ba74a3a4dd637ce5d612de9aaecff28724
IstokCarvallo <51374996+IstokCarvallo@users.noreply.github.com>
2024-10-03 09:40:08 -0300

    Carga en Mysql y cambio a coneccion produccion

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def insertar(mensaje):
     return str(f'{datetime.datetime.now()} - {mensaje}\n')
 
 def archivo(mensaje):
-    file = f'LOG_{datetime.datetime.now().strftime("%d%m%YT%H%M")}.TXT'
+    file = f'LOG_{datetime.datetime.now().strftime("%d%m%YT%H%M")}.LOG'
 
     file = open(file, "w")
     file.write(mensaje)
@@ -28,11 +28,11 @@ def conecta_SQL():
 def conecta_MySQL():
     try:
         coneccion = mysql.connector.connect(
-            host='localhost',
+            host='192.168.20.5',
             port=3306,
-            user='istok',
-            password='10Bh0q10*',
-            db='World'
+            user='Uprag',
+            password='rio.2024*',
+            db='pallet_24'
         )
     except Error as ex:
         print(f"Error durante la coneccion:{format(ex)}")
@@ -43,6 +43,8 @@ def conecta_MySQL():
 
 def cargar():
     mensaje = ''
+    fecha = '20240927' # datetime.datetime.now().strftime("%Y%m%d")
+
     mensaje += insertar('Inicio de Proceso de Carga...')
     mensaje += insertar('Recuperacion de Despachos a cargar...')
 
@@ -51,7 +53,7 @@ def cargar():
     if conectado:
         produccion = conexion.cursor()
 
-        storeproc = "execute dbo.frusys_despachos '20240927'"
+        storeproc = f"execute dbo.frusys_despachos '{fecha}'"
         produccion.execute(storeproc)
         despachos = produccion.fetchall()
 
@@ -60,29 +62,40 @@ def cargar():
         if mySQL.is_connected():
             frusys = mySQL.cursor()
 
+            elimina = (f"Delete From despacho "
+                       f"Where Datediff(fechaDespacho, '{fecha}') = 0 "
+                       f"Or Datediff(fechaDespacho, '{fecha}') = 1")
+            
+            mensaje += insertar(f'Se eliminaran {frusys.rowcount} registros.')
+
+            frusys.execute(elimina)
+            mySQL.commit()
+            mensaje += insertar('Registros Elimiandos.')
+                                
+            mensaje += insertar(f'Se cargaran {len(despachos)} registros.')
+        
+
             for despacho in despachos:
                 sql = ("Insert Into Despacho(operacion,embarque,planta,consignatario,fechaDespacho,guia,"
                 "contenedor,folio,especie,variedad,variedadReal,embalaje,etiqueta,productor,predio,"
                 "cuartel,productorReal,predioReal,cuartelReal,packing,fechaEmbalaje,categoria,"
                 "categoriaReal,calibre,calibreReal,condicion,basePallet,cajas)"
-                f"Values ({despacho[0]}, '{despacho[1]}', {despacho[2]}, {despacho[3]}, {despacho[4]},"
-                f"{despacho[5]}, '{despacho[6]}', '{despacho[7]}', {despacho[8]}, {despacho[9]}, {despacho[10]},"
-                f"'{despacho[11]}', {despacho[12]}, {despacho[13]}, {despacho[14]}, {despacho[15]}, {despacho[16]},"
-                f"{despacho[17]}, {despacho[18]}, {despacho[19]}, {despacho[20]}, {despacho[21]}, {despacho[22]},"
+                f"Values ({despacho[0]}, '{despacho[1]}', {despacho[2]}, {despacho[3]}, '{despacho[4]}', "
+                f"{despacho[5]}, '{despacho[6]}', '{despacho[7]}', {despacho[8]}, {despacho[9]}, {despacho[10]}, "
+                f"'{despacho[11]}', {despacho[12]}, {despacho[13]}, {despacho[14]}, {despacho[15]}, {despacho[16]}, "
+                f"{despacho[17]}, {despacho[18]}, {despacho[19]}, '{despacho[20]}', {despacho[21]}, {despacho[22]}, "
                 f"'{despacho[23]}', '{despacho[24]}', '{despacho[26]}', {despacho[25]}, {despacho[27]})")
-                print(sql)
+                
+                frusys.execute(sql)
+                mySQL.commit()
 
-                # frusys.execute(sql)
-
-            print(frusys.rowcount)
-            print(len(despachos))
-            
         mySQL.close()
 
         produccion.close()
         conexion.close()
 
-        # archivo(mensaje)
+        mensaje += insertar('Termino de carga Despachos...')
+        archivo(mensaje)
 
 
 

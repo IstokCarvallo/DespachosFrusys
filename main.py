1ae0610148d6f5fc345fb49d5adda2198aac8490
istok.carvallo@rioblanco.net <istok.carvallo@rioblanco.net>
2025-01-28 17:34:00 -0300

    Se agrega Tipo Planilla

--- main.py
+++ main.py
@@ -73,19 +73,19 @@ def cargar():
             mySQL.commit()
             mensaje += insertar('Registros Elimiandos.')
                                 
-            mensaje += insertar(f'Se cargaran {len(despachos)} registros.')
-        
+            mensaje += insertar(f'Se cargaran {len(despachos)} registros.') 
 
             for despacho in despachos:
                 sql = ("Insert Into Despacho(operacion,embarque,planta,consignatario,fechaDespacho,guia,"
                 "contenedor,folio,especie,variedad,variedadReal,embalaje,etiqueta,productor,predio,"
                 "cuartel,productorReal,predioReal,cuartelReal,packing,fechaEmbalaje,categoria,"
-                "categoriaReal,calibre,calibreReal,condicion,basePallet,cajas,codigo_operacional)"
+                "categoriaReal,calibre,calibreReal,condicion,basePallet,cajas,codigo_operacional,Tipo_Planilla_C, Tipo_Planilla)"
                 f"Values ('{despacho[0]}', '{despacho[1]}', {despacho[2]}, {despacho[3]}, '{despacho[4]}', "
                 f"{despacho[5]}, '{despacho[6]}', '{despacho[7]}', {despacho[8]}, {despacho[9]}, {despacho[10]}, "
                 f"'{despacho[11]}', {despacho[12]}, {despacho[13]}, {despacho[14]}, {despacho[15]}, {despacho[16]}, "
                 f"{despacho[17]}, {despacho[18]}, {despacho[19]}, '{despacho[20]}', {despacho[21]}, {despacho[22]}, "
-                f"'{despacho[23]}', '{despacho[24]}', '{despacho[26]}', {despacho[25]}, {despacho[27]}, '{despacho[28]}')")
+                f"'{despacho[23]}', '{despacho[24]}', '{despacho[26]}', {despacho[25]}, {despacho[27]}, '{despacho[28]}', "
+                f"'{despacho[29]}', '{despacho[30]}')")
                 
                 frusys.execute(sql)
                 mySQL.commit()
